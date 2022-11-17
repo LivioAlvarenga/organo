30191cc14e0c9a35c8e36e95a59f1b6147a59c9f
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-17 14:49:53 -0300

    instalado tailwindcss no projeto

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        principal: {
+          100: "#6278f7",
+          200: "#95ffd4",
+          300: "#f2f2f2",
+        },
+        fundos: {
+          100: "#D9F7E9",
+          200: "#E8F8FF",
+          300: "#F0F8E2",
+          400: "#FDE7E8",
+          500: "#FAE9F5",
+          600: "#FFF5D9",
+          700: "#ffeedf",
+        },
+        destaques: {
+          100: "#57C278",
+          200: "#82CFFA",
+          300: "#A6D157",
+          400: "#E06B69",
+          500: "#DB6EBF",
+          600: "#FFBA05",
+          700: "#FF8A29",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
